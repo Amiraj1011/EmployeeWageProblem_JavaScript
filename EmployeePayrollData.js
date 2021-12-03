5b0788a7ff691a68b6281f4ba21f31bc91c49bf3
“AMIR <amiraj1011@gmail.com>
2021-12-02 16:18:51 -0800

    [amir] added to extend data to store gender and start date

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -3,13 +3,17 @@ class EmployeePayrollData
     //property
     id;
     salary;
+    gender;
+    startDate;
 
     //constructor
-    constructor (id, name, salary)
+    constructor (...params)
     {
-        this.id =id;
-        this.name = name;
-        this.salary = salary;
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3]
+        this.startDate = params[4];
     }
     //getter and setter method
     get name() {return this._name;}
@@ -23,4 +27,5 @@ class EmployeePayrollData
 }
 let EmployeePayroll = new EmployeePayrollData(1, "Amir",33333333);
 console.log(EmployeePayroll.toString());
-
+let newEmployeePayroll = new EmployeePayrollData(3,"AJ",3333333,"M",new Date());
+console.log(newEmployeePayroll);
